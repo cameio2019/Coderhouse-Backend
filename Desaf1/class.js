ac9419b3265a678970dc8477e29fd6eb9a59e13f
Zeckedev <hlfoeste@gmail.com>
2021-10-23 00:09:07 -0400

    update1

--- Desaf1/class.js
+++ Desaf1/class.js
@@ -28,7 +28,7 @@ class Usuario {
     }
 }
 
-const usuario = new Usuario('Eze', 'Arevalo',[], ['Glan']);
+const usuario = new Usuario('Eze', 'Arevalo',[], []);
 
 console.log(usuario.getFullName());
 
@@ -38,6 +38,7 @@ usuario.getFullName()//retorna nombre completo del usuario.
 usuario.addBook('Aerodinamica', 'Juan Carmona')//Recibe un string nombre y autor y lo agrega  al array de libros del usuario.
 usuario.addBook('Vuelo nocturno', 'Saint Exupery')
 usuario.addMascota('Juano')//recibe un nombre y agrega una mascota al array de mascotas.
+usuario.addMascota('Glan')
 
 console.log(usuario);
 console.log(usuario.countMascota());
